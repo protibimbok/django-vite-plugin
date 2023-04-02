cc8db0c18a587f7d73328f9809ad5de613524d0c
Saad <sakib.saad.khan@gmail.com>
2023-04-02 23:39:05 +0600

    added test files to input

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import djangoVite from './django-vite-plugin/index.js'
 import vue from '@vitejs/plugin-vue'
+import glob from 'glob'
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,10 @@ export default defineConfig({
             input:[
                 'home/js/app.js',
                 'home/css/tailwind.css',
+
+                //index.html
+                'another_app/js/one.js',
+                ...glob.sync('static/**/*.{js,css}'),
             ]
         })
     ],
