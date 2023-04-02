b4dee04b134613b7f4c65a0fd8b9bf3788d1a07e
Saad <sakib.saad.khan@gmail.com>
2023-04-02 15:20:00 +0600

    changed indentation 2 -> 4

--- example/home/static/js/app.js
+++ example/home/static/js/app.js
@@ -1,26 +1,26 @@
 import Swal from "sweetalert2";
 import { find } from "./utils";
-import { createApp } from 'vue'
-import VueCounter from '@/home/templates/vue/counter.vue'
+import { createApp } from "vue";
+import VueCounter from "@/home/templates/vue/counter.vue";
 
 find("#next-move").addEventListener("click", () => {
-  let timerInterval;
-  Swal.fire({
-    title: "ChatGPT is going to take your job, what are going to do about it?!",
-    html: "Time to decide: <b></b>.",
-    timer: 2000,
-    timerProgressBar: true,
-    didOpen: () => {
-      Swal.showLoading();
-      const b = Swal.getHtmlContainer().querySelector("b");
-      timerInterval = setInterval(() => {
-        b.textContent = Swal.getTimerLeft();
-      }, 100);
-    },
-    willClose: () => {
-      clearInterval(timerInterval);
-    },
-  })
+    let timerInterval;
+    Swal.fire({
+        title: "ChatGPT is going to take your job, what are going to do about it?!",
+        html: "Time to decide: <b></b>.",
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const b = Swal.getHtmlContainer().querySelector("b");
+            timerInterval = setInterval(() => {
+                b.textContent = Swal.getTimerLeft();
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        },
+    });
 });
 
-createApp(VueCounter).mount('#vue-app')
+createApp(VueCounter).mount("#vue-app");
