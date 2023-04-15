caa25f2b8b14ea4ffeeb9bfa95db30541765f4c0
Saad <sakib.saad.khan@gmail.com>
2023-04-16 01:43:32 +0600

    used built-in static finder

--- example/home/views.py
+++ example/home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     return render(req, 'index.html', {
-        'dyn_js' : 'a_dynamic_js.js',
-        'dyn_css' : 'a_dynamic_css.css',
+        'dyn_js' : 'static/a_dynamic_js.js',
+        'dyn_css' : 'static/a_dynamic_css.css',
         'dyn_attr': 'A dynamic attr'
     })
