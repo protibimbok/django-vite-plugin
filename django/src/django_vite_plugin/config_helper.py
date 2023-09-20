89e11ec52210b1ab1b26a3f4890fa9401d175f7a
Daniel Niccoli <2971735+danielniccoli@users.noreply.github.com>
2023-09-20 23:30:19 +0200

    Update config_helper.py

--- django/src/django_vite_plugin/config_helper.py
+++ django/src/django_vite_plugin/config_helper.py
@@ -1,12 +1,12 @@
-from django.conf import settings
+from django.conf import settings, global_settings
 
 
 # MANIFEST_PATH = Path(MANIFEST_PATH)
 DEFAULT = {
     'WS_CLIENT': '@vite/client',
-    'DEV_MODE': getattr(settings, 'DEBUG', True),
-    'BUILD_DIR': getattr(settings, 'STATIC_ROOT') or 'static',
-    'BUILD_URL_PREFIX': getattr(settings, 'STATIC_URL'), # Manifest paths would be prefixed with this
+    'DEV_MODE': getattr(settings, 'DEBUG', global_settings.DEBUG),
+    'BUILD_DIR': getattr(settings, 'STATIC_ROOT', global_settings.STATIC_ROOT),
+    'BUILD_URL_PREFIX': getattr(settings, 'STATIC_URL', global_settings.STATIC_URL), # Manifest paths would be prefixed with this
     'SERVER': {
         'HTTPS': False,
         'HOST': '127.0.0.1',
