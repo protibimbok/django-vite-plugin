ab0344660758abbeaf3958dfae704aec19849e76
Saad <sakib.saad.khan@gmail.com>
2023-09-27 15:00:01 +0600

    added inter fonts

--- example/home/static/home/js/app.js
+++ example/home/static/home/js/app.js
@@ -4,6 +4,8 @@ import { showAlert } from '@s:home/js/deep/deep.js'
 import VueCounter from '@t:home/vue/counter.vue'
 import '@s:home/css/imported.css'
 
+import "inter-ui/inter.css"
+
 find("#next-move").addEventListener("click", showAlert);
 
 createApp(VueCounter).mount("#vue-app");
