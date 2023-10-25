3f5055747a5767ffbe39840196c8a73b48fe9de0
Saad <sakib.saad.khan@gmail.com>
2023-10-25 12:03:08 +0600

    generate warning

--- example/home/models.py
+++ example/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TestModel(models.Model):
+    name = models.TextField()
+
+class TestModel2(models.Model):
+    categories= models.ManyToManyField(TestModel, blank=True, null=True)
