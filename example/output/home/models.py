5d79dd46f6821a02d73df8fbe553a9c18523a170
Sakibur Rahman Khan <sakib.saad.khangmail.com>
2023-12-16 16:23:56 +0600

    output example is done

--- example/output/home/models.py
+++ example/output/home/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-"""
-class TestModel(models.Model):
-    name = models.TextField()
-
-class TestModel2(models.Model):
-    categories= models.ManyToManyField(TestModel, blank=True, null=True)
-"""
--- example/output/home/static/home/js/app.js
+++ example/output/home/static/home/js/app.js
@@ -1,3 +1,1 @@
 import '@s:home/css/imported.css'
-
-import "inter-ui/inter.css"
